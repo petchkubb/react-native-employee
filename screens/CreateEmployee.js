af2c421e0d85cbb606d378baa14fea49a0e52d7c
Petch <petchkubb@live.com>
2020-07-15 22:33:00 +0700

    create modal component

--- screens/CreateEmployee.js
+++ screens/CreateEmployee.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import {TextInput} from 'react-native-paper';
+import {View, StyleSheet, Modal, SafeAreaView} from 'react-native';
+import {TextInput, Button} from 'react-native-paper';
 
 const CreateEmployee = () => {
   const [name, setName] = useState('');
@@ -45,6 +45,49 @@ const CreateEmployee = () => {
         theme={theme}
         style={styles.inputStyle}
       />
+      <Button
+        style={styles.inputStyle}
+        icon="upload"
+        mode="contained"
+        onPress={() => setModal(true)}
+        theme={theme}>
+        Upload Image
+      </Button>
+      <Button
+        style={styles.inputStyle}
+        icon="content-save"
+        mode="contained"
+        onPress={() => {}}
+        theme={theme}>
+        Save
+      </Button>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modal}
+        onRequestClose={() => setModal(false)}>
+        <SafeAreaView style={styles.modalView}>
+          <View style={styles.modalButtonView}>
+            <Button
+              icon="upload"
+              mode="contained"
+              onPress={() => setModal(false)}
+              theme={theme}>
+              Camera
+            </Button>
+            <Button
+              icon="image-area"
+              mode="contained"
+              onPress={() => setModal(false)}
+              theme={theme}>
+              Gallary
+            </Button>
+          </View>
+          <Button icon="upload" onPress={() => setModal(false)}>
+            Cancel
+          </Button>
+        </SafeAreaView>
+      </Modal>
     </View>
   );
 };
@@ -61,6 +104,17 @@ const styles = StyleSheet.create({
   },
   inputStyle: {
     margin: 5,
+  },
+  modalButtonView: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10,
+  },
+  modalView: {
+    position: 'absolute',
+    bottom: 2,
+    width: '100%',
+    backgroundColor: 'white',
   },
 });
 
