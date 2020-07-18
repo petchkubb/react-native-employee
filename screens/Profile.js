7b1413bf7050fb48185e3a471d34e2c753d60d3a
Petch <petchkubb@live.com>
2020-07-18 18:37:09 +0700

    add profile screen part 2

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {View, Text, StyleSheet, Image} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import {Title, Card, Button} from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const Profile = () => {
   return (
     <View style={styles.root}>
       <LinearGradient colors={['#0033ff', '#6bc1ff']} style={{height: '20%'}} />
-      <View style={{alignItems: 'center'}}>
+      <View style={styles.center}>
         <Image
           source={{
             uri:
@@ -15,8 +17,48 @@ const Profile = () => {
           style={styles.imageStyle}
         />
       </View>
+      <View style={styles.center}>
+        <Title>Jame McCarthy</Title>
+        <Text style={{fontSize: 18}}>Web Developer</Text>
+      </View>
+      <Card style={styles.mycard}>
+        <View style={styles.cardContent}>
+          <Icon name="email" size={32} color="#006aff" />
+          <Text style={styles.text}>abc@mail.com</Text>
+        </View>
+      </Card>
+      <Card style={styles.mycard}>
+        <View style={styles.cardContent}>
+          <Icon name="call" size={32} color="#006aff" />
+          <Text style={styles.text}>0920070156</Text>
+        </View>
+      </Card>
+      <Card style={styles.mycard}>
+        <View style={styles.cardContent}>
+          <Icon name="attach-money" size={32} color="#006aff" />
+          <Text style={styles.text}>10000 ฿</Text>
+        </View>
+      </Card>
+      <View style={styles.buttonContainer}>
+        <Button
+          icon="account-edit"
+          mode="contained"
+          theme={theme}
+          onPress={() => {}}>
+          Edit
+        </Button>
+        <Button icon="delete" mode="contained" theme={theme} onPress={() => {}}>
+          Fire Employee
+        </Button>
+      </View>
     </View>
   );
+};
+
+const theme = {
+  colors: {
+    primary: '#006aff',
+  },
 };
 
 const styles = StyleSheet.create({
@@ -28,6 +70,27 @@ const styles = StyleSheet.create({
     height: 140,
     borderRadius: 140 / 2,
     marginTop: -50,
+  },
+  center: {
+    alignItems: 'center',
+    margin: 15,
+  },
+  mycard: {
+    margin: 3,
+  },
+  cardContent: {
+    flexDirection: 'row',
+    padding: 8,
+  },
+  text: {
+    fontSize: 18,
+    marginTop: 5,
+    marginLeft: 5,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10,
   },
 });
 
