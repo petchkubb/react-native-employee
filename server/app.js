b682f43a5477fbcaf40a02226d0f861313476322
Petch <petchkubb@live.com>
2020-07-19 14:14:24 +0700

    add post method

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require('./Employee');
 
+app.use(bodyParser.json());
+
 const Employee = mongoose.model('employee');
 
 const mongoUrl =
@@ -19,6 +21,27 @@ mongoose.connection.on('error', (err) => console.log('error', err));
 
 app.get('/', (req, res) => {
   res.send('welcome to node js');
+});
+
+app.post('/send-data', (req, res) => {
+  const {name, email, phone, picture, salary, position} = req.body;
+  const employee = new Employee({
+    name,
+    email,
+    phone,
+    picture,
+    salary,
+    position,
+  });
+  employee
+    .save()
+    .then((data) => {
+      console.log(data);
+      res.send('success');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.listen(3000, () => {
